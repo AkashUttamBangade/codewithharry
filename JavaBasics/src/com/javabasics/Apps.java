1f40310ded7e376b7cbcc25cab5573da0d4c1e24
akash <aubangde@gmail.com>
2024-03-06 16:55:17 +0530

    Added Java Notes file

---
+++ JavaBasics/src/com/javabasics/Apps.java
@@ -0,0 +1,12 @@
+package com.javabasics;		//Group of related class e.g. Folder
+
+public class Apps {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Application Started ...");
+		//System.out.println(args[0]);
+	}
+
+}
